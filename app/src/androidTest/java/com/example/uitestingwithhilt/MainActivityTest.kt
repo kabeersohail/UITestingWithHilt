27905e61cbfbf840e1e34525c18543ae0816a878
Sohail <kabeersohel@gmail.com>
2022-08-07 03:47:49 +0530

    This is initially running

--- app/src/androidTest/java/com/example/uitestingwithhilt/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/uitestingwithhilt/MainActivityTest.kt
@@ -2,14 +2,11 @@ package com.example.uitestingwithhilt
 
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.HiltTestApplication
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
-import org.robolectric.annotation.Config
 
 @HiltAndroidTest
-@Config(application = HiltTestApplication::class)
 class MainActivityTest {
 
     @get:Rule
