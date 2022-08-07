ef91f8a8ce3dc2ebdbf92c9e377e0027057a2aff
Sohail <kabeersohel@gmail.com>
2022-08-07 12:30:28 +0530

    activity is visible test working

--- app/src/androidTest/java/com/example/uitestingwithhilt/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/uitestingwithhilt/MainActivityTest.kt
@@ -1,5 +1,10 @@
 package com.example.uitestingwithhilt
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Assert
@@ -15,6 +20,12 @@ class MainActivityTest {
     @Test
     fun firstTest() {
         Assert.assertEquals(1,1)
+    }
+
+    @Test
+    fun test_if_main_activity_is_visible() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.main_activity_layout)).check(matches(isDisplayed()))
     }
 
 }
