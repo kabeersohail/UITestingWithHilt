69c1120fb3f4854bc8d443661eac72477b7dcc56
Sohail <kabeersohel@gmail.com>
2022-08-07 13:01:17 +0530

    added activityScenarioRule, as a single rule that suffices launching of activity for all tests.
    Fix took lot of time and needed dependency corrections

--- app/src/androidTest/java/com/example/uitestingwithhilt/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/uitestingwithhilt/MainActivityTest.kt
@@ -1,9 +1,9 @@
 package com.example.uitestingwithhilt
 
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.activityScenarioRule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Assert
@@ -13,8 +13,11 @@ import org.junit.Test
 @HiltAndroidTest
 class MainActivityTest {
 
-    @get:Rule
+    @get:Rule(order = 0)
     var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
 
     @Test
     fun firstTest() {
@@ -23,13 +26,17 @@ class MainActivityTest {
 
     @Test
     fun test_if_main_activity_is_visible() {
-        ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.main_activity_layout)).check(matches(isDisplayed()))
     }
 
     @Test
+    fun test_if_main_activity_text_is_displayed() {
+        onView(withId(R.id.main_activity_text)).check(matches(isDisplayed())) // method 1 to determine if text is visible
+        onView(withId(R.id.main_activity_text)).check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // // method 2 to determine if text is visible
+    }
+
+    @Test
     fun test_if_main_activity_text_matches_as_expected() {
-        ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.main_activity_text)).check(matches(withText(R.string.main_activity)))
     }
 
